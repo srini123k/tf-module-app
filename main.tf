7a8c07eebecfdaa3e61b3b51a154585c979c0226
Srini K <srinikdevops0131@gmail.com>
2023-03-27 22:38:10 -0700

    updating ./tf-module-app

--- main.tf
+++ main.tf
@@ -110,18 +110,18 @@ resource "aws_route53_record" "main" {
   records = [var.alb_dns_name]
 }
 
-#resource "aws_lb_listener_rule" "listener_rule" {
-#  listener_arn = var.listener_arn
-#  priority     = var.listener_priority
-#
-#  action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.main.arn
-#  }
-#
-#  condition {
-#    host_header {
-#      values = [local.dns_name]
-#    }
-#  }
-#}
+resource "aws_lb_listener_rule" "listener_rule" {
+  listener_arn = var.listener_arn
+  priority     = var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.dns_name]
+    }
+  }
+}

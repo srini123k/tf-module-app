fef82fc622d379eeb03303cf168d89f8aec3a25d
Srini K <srinikdevops0131@gmail.com>
2023-03-27 22:07:44 -0700

    updating ./tf-module-app

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_autoscaling_group" "main" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnets
-#  target_group_arns   = [aws_lb_target_group.main.arn]
+  target_group_arns   = [aws_lb_target_group.main.arn]
 
 
   launch_template {
@@ -84,24 +84,24 @@ resource "aws_security_group" "main" {
   )
 }
 
-#resource "aws_lb_target_group" "main" {
-#  name     = "${var.component}-${var.env}"
-#  port     = var.port
-#  protocol = "HTTP"
-#  vpc_id   = var.vpc_id
-#  health_check {
-#    enabled             = true
-#    healthy_threshold   = 2
-#    unhealthy_threshold = 5
-#    interval            = 5
-#    timeout             = 4
-#  }
-#  tags = merge(
-#    var.tags,
-#    { Name = "${var.component}-${var.env}" }
-#  )
-#}
-#
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}"
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+    interval            = 5
+    timeout             = 4
+  }
+  tags = merge(
+    var.tags,
+    { Name = "${var.component}-${var.env}" }
+  )
+}
+
 #resource "aws_route53_record" "main" {
 #  zone_id = data.aws_route53_zone.domain.zone_id
 #  name    = local.dns_name
